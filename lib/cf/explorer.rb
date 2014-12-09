257a91afd9f9e4093a450c6b35ce95e7d3653520
Ben Laplanche <ben@laplanche.co.uk>
2014-12-09 21:04:33 +0000

    basic tests for creating a client

--- lib/cf/explorer.rb
+++ lib/cf/explorer.rb
@@ -2,6 +2,16 @@ require "cf/explorer/version"
 
 module CF
   module Explorer
-    # Your code goes here...
+  	class Client
+	  	attr_reader :user, :password, :api_url, :uaa_url, :skip_ssl_validation
+
+	  	def initialize(options={})
+	  		@user 								= options.fetch(:user, "admin")
+	  		@password 						= options.fetch(:password)
+	  		@uaa_url 							= options.fetch(:uaa_url)
+	  		@api_url 							= options.fetch(:api_url)
+	  		@skip_ssl_validation 	= options.fetch(:options).fetch(:skip_ssl_validation)
+	  	end
+  	end
   end
 end
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -1,8 +1,28 @@
 require 'spec_helper'
 
-describe "Creating a client" do
+describe "Cloud Foundry Explorer" do
 
 	it "should have the correct version" do
 		expect(CF::Explorer::VERSION).to eq('0.0.1')
+	end
+
+	context "creating a client" do
+		let(:client) { CF::Explorer::Client.new(
+			uaa_url: 	ENV['CF_UAA'],
+			api_url: 	ENV['CF_API'],
+			user: 		ENV['CF_USER'],
+			password: ENV['CF_PASSWORD'],
+			options: {
+				skip_ssl_validation: true
+			})
+		}
+
+		it "should return the correct values" do
+			expect(client.uaa_url).to 						eq(ENV['CF_UAA'])
+			expect(client.api_url).to 						eq(ENV['CF_API'])
+			expect(client.user).to 								eq(ENV['CF_USER'])
+			expect(client.password).to 						eq(ENV['CF_PASSWORD'])
+			expect(client.skip_ssl_validation).to eq(true)
+		end
 	end
 end
